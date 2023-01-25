8e6ba0d694b368761da4daad22c790956491c6e5
Volpe <renan.volpe@aluno.ifsp.edu.br>
2023-01-25 13:47:22 -0300

    creating animal s page to start to show

--- lib/features/animals/pages/home_animals_page.dart
+++ lib/features/animals/pages/home_animals_page.dart
@@ -1,3 +1,4 @@
+import 'package:adopt_pet_app/features/animals/components/animal_section.dart';
 import 'package:flutter/material.dart';
 
 class HomeAnimalsPage extends StatefulWidget {
@@ -10,8 +11,50 @@ class HomeAnimalsPage extends StatefulWidget {
 class _HomeAnimalsPageState extends State<HomeAnimalsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Primeira página animals"),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("adote um animal"),
+        ),
+        body: Stack(
+          children: [
+            Image.asset(
+              color: Colors.white.withOpacity(0.75),
+              colorBlendMode: BlendMode.modulate,
+              height: double.infinity,
+              width: double.infinity,
+              "assets/images/image_bulma.jpeg",
+              fit: BoxFit.cover,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text("Selecione a adoção"),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AnimalSection(
+                  image: "assets/images/dog_option.png",
+                  text: "Adote um cachorro",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AnimalSection(
+                  image: "assets/images/cat_option.png",
+                  text: "Adote um gato",
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const AnimalSection(
+                  image: "assets/images/dog_cat_option.png",
+                  text: "Adote um cachorro ou gato",
+                ),
+                Row()
+              ],
+            )
+          ],
+        ));
   }
 }
